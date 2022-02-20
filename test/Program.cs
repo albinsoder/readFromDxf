24640d90f3df16fd856dd4af907b85d3751e4643
Albin Södervall <91470009+albinsoder@users.noreply.github.com>
2022-02-20 21:38:58 +0100

    Add files via upload

---
+++ test/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System;
+using System.Text;
+using System.Drawing.Drawing2D;
+using System.Data;
+using System.Collections.Generic;
+
+namespace file
+{
+  class Program{
+
+    static void Main(string[] args){
+
+      string DxfFile = "Skeppsbrokajen.dxf";
+      string csvFile = "Michanotest.csv";
+
+      List<string> lines = new List<string>();
+      List<string> lines2 = new List<string>();
+      lines2.Add("y,x,z");
+      lines = File.ReadAllLines(DxfFile).ToList();
+      int size = lines.Count();
+      for(int i=0; i<size; i++){
+          if(lines[i] == "AcDb3dPolylineVertex"){
+              if(lines[i+6] != "-999.0"){
+                string xyz = lines[i+4] + ","+lines[i+2] +"," + lines[i+6];
+                lines2.Add(xyz);
+                File.WriteAllLines(csvFile, lines2);
+              }
+          }
+      }
+    }
+  }
+}
+ 
+
